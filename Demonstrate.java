313cb6961dddf1e09f643f0b778d459833650b7a
Adil <adilchhabra7@gmail.com>
2016-03-14 15:49:19 -0400

    Swapped for stored int values

--- Demonstrate.java
+++ Demonstrate.java
@@ -2,7 +2,7 @@ public class Demonstrate {
 	public static void main(String [] args) {
 		int script=6, acting=9, direction=8; 
 		System.out.println("The Rating of the movie is: "); 
-		System.out.println(movieRating(6,9,8));
+		System.out.println(movieRating(script,acting,direction));
 	}
 
 	public static int movieRating (int s, int a, int d) { 
