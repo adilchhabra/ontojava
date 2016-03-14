576975b54f5958ea43d07e881c76c3fd1f674267
Adil <adilchhabra7@gmail.com>
2016-03-13 23:47:34 -0400

    Alternate Formatting

--- Demonstrate.java
+++ Demonstrate.java
@@ -1,8 +1,6 @@
 public class Demonstrate {
 	public static void main(String [] args) {
-		int script; 
-  		int acting; 
-  		int direction; 
+		int script, acting, direction; 
 		System.out.println("The Rating of the movie is: "); 
 		System.out.println(6+9+8);
 	}
