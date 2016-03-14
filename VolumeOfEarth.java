5bf07794aa5b7dff1bd2d97554c8c90cb8e12e31
Adil <adilchhabra7@gmail.com>
2016-03-13 23:16:40 -0400

    Practice Program 1

---
+++ VolumeOfEarth.java
@@ -0,0 +1,6 @@
+public class VolumeOfEarth {
+	public static void main(String [] args) {
+	System.out.println("The volume of the earth is: ");
+	System.out.println((4/3.0)*3.14*6400000*6400000*6400000);
+	}
+}
