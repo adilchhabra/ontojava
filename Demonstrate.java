27685beb391a3a4051dca616594543064dc34ca9
Adil <adilchhabra7@gmail.com>
2016-03-14 15:52:18 -0400

    Void Variation

--- Demonstrate.java
+++ Demonstrate.java
@@ -1,11 +1,11 @@
 public class Demonstrate {
 	public static void main(String [] args) {
 		int script=6, acting=9, direction=8; 
-		System.out.println("The Rating of the movie is: "); 
-		System.out.println(movieRating(script,acting,direction));
+		displayMovieRating(script,acting,direction);
 	}
 
-	public static int movieRating (int s, int a, int d) { 
-  	return s + a + d; 
+	public static void displayMovieRating (int s, int a, int d) { 
+  	System.out.println("The rating of the movie is: ");
+  	System.out.println(s+a+d);
  	} 
 }
