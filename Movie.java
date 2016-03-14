7c8ea7df9ec3621609ed6e346a811c094c38ef8a
Adil <adilchhabra7@gmail.com>
2016-03-14 19:03:12 -0400

    Local Variable

--- Movie.java
+++ Movie.java
@@ -1,7 +1,8 @@
 public class Movie {
 	
 	public static void displayMovieRating (int s, int a, int d) { 
+	int result = s+a+d;
   	System.out.println("The rating of the movie is: ");
-  	System.out.println(s+a+d);
+  	System.out.println(result);
  	} 
 }
