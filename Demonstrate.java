842248bfe89ee25e116fc8bdf2a2182bc8e64000
Adil <adilchhabra7@gmail.com>
2016-03-14 16:00:55 -0400

    Added Floating Number Method.
    Method has same name. Not an error as different parameters.

--- Demonstrate.java
+++ Demonstrate.java
@@ -1,7 +1,13 @@
 public class Demonstrate {
 	public static void main(String [] args) {
 		int script=6, acting=9, direction=8; 
+		double doubleScript = 6.0, doubleActing = 9.0, doubleDirection = 8.0; 
 		Movie.displayMovieRating(script,acting,direction);
+		displayMovieRating(doubleScript, doubleActing, doubleDirection); 
 	}
-
+	public static void displayMovieRating (double s, double a, double d) { 
+  	System.out.print("The floating-point rating of the movie is ");  
+  	System.out.println(s + a + d);  
+  	return; 
+ }  
 }
