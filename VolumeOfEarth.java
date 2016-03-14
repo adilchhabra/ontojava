9ac38e340252887b55ec8e5adafb4627d70dcce5
Adil <adilchhabra7@gmail.com>
2016-03-14 00:04:00 -0400

    Variable Replacement

--- VolumeOfEarth.java
+++ VolumeOfEarth.java
@@ -1,6 +1,7 @@
 public class VolumeOfEarth {
 	public static void main(String [] args) {
+	int r = 6400000;
 	System.out.println("The volume of the earth is: ");
-	System.out.println((4/3.0)*3.14*6400000*6400000*6400000);
+	System.out.println((4/3.0)*3.14*r*r*r);
 	}
 }
