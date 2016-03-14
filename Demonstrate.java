f882f3df8b673214e9a61f75169f5dd7034adcd4
Adil <adilchhabra7@gmail.com>
2016-03-14 15:55:20 -0400

    Creation of Movie Class
    Added and called from another class.

--- Demonstrate.java
+++ Demonstrate.java
@@ -1,11 +1,7 @@
 public class Demonstrate {
 	public static void main(String [] args) {
 		int script=6, acting=9, direction=8; 
-		displayMovieRating(script,acting,direction);
+		Movie.displayMovieRating(script,acting,direction);
 	}
 
-	public static void displayMovieRating (int s, int a, int d) { 
-  	System.out.println("The rating of the movie is: ");
-  	System.out.println(s+a+d);
- 	} 
 }
---
+++ Movie.java
@@ -0,0 +1,7 @@
+public class Movie {
+	
+	public static void displayMovieRating (int s, int a, int d) { 
+  	System.out.println("The rating of the movie is: ");
+  	System.out.println(s+a+d);
+ 	} 
+}
