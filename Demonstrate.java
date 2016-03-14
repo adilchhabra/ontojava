e5036f9fd7de96328681c94c1ea5d9eef01c0e24
Adil <adilchhabra7@gmail.com>
2016-03-13 23:50:27 -0400

    Initialized Values

--- Demonstrate.java
+++ Demonstrate.java
@@ -1,6 +1,6 @@
 public class Demonstrate {
 	public static void main(String [] args) {
-		int script, acting, direction; 
+		int script=6, acting=9, direction=8; 
 		System.out.println("The Rating of the movie is: "); 
 		System.out.println(6+9+8);
 	}
