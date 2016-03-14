bfa14baf7c17823d06aa64c1a257748148a7a2f3
Adil <adilchhabra7@gmail.com>
2016-03-14 00:41:23 -0400

    Defined Method

--- Demonstrate.java
+++ Demonstrate.java
@@ -2,6 +2,10 @@ public class Demonstrate {
 	public static void main(String [] args) {
 		int script=6, acting=9, direction=8; 
 		System.out.println("The Rating of the movie is: "); 
-		System.out.println(6+9+8);
+		System.out.println(movieRating(6,9,8));
 	}
+
+	public static int movieRating (int s, int a, int d) { 
+  	return s + a + d; 
+ 	} 
 }
