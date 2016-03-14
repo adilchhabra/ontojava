e2d522fa95f18737fe57c7d1872105df8eea0af6
Adil <adilchhabra7@gmail.com>
2016-03-13 23:46:52 -0400

    Declared variables

--- Demonstrate.java
+++ Demonstrate.java
@@ -1,6 +1,9 @@
 public class Demonstrate {
 	public static void main(String [] args) {
-		System.out.println("The Rating of the movie is: ");
+		int script; 
+  		int acting; 
+  		int direction; 
+		System.out.println("The Rating of the movie is: "); 
 		System.out.println(6+9+8);
 	}
 }
