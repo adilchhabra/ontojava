1d283398fbae113c69f9f8b25d18411231a90406
Adil <adilchhabra7@gmail.com>
2016-03-14 20:27:56 -0400

    Calling New Method - rating

--- Demonstrate.java
+++ Demonstrate.java
@@ -2,7 +2,8 @@ public class Demonstrate {
 	public static void main(String [] args) {
 		Movie m = new Movie(); 
 		int m.script=6, m.acting=9, m.direction=8; 
-		Movie.displayMovieRating(m.script,m.acting,m.direction);
+		System.out.print("The rating of the movie is "); 
+  		System.out.println(Movie.rating(m)); 
 	}
 	
 }
