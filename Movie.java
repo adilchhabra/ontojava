635e1a0ede93aefbab3469ebb9bd4d5c1c0c0f1a
Adil <adilchhabra7@gmail.com>
2016-03-14 21:00:23 -0400

    Added Standard Constructor

--- Movie.java
+++ Movie.java
@@ -1,6 +1,9 @@
 public class Movie {
- 	public int script, acting, direction; 
- 	public int rating () { 
+ 	public int script, acting, direction;
+ 	public Movie(){
+ 		script = 5; acting = 5; direction = 5;        
+ 	} 
+ 	public static int rating () { 
   		return script + acting + direction; 
  } 
 } 
