e02ff0dfb1f728257996c32d51083b51f4cc4dd3
Adil <adilchhabra7@gmail.com>
2016-03-14 20:24:24 -0400

    Utilizing Public Instance Variables

--- Demonstrate.java
+++ Demonstrate.java
@@ -1,13 +1,8 @@
 public class Demonstrate {
 	public static void main(String [] args) {
-		int script=6, acting=9, direction=8; 
-		double doubleScript = 6.0, doubleActing = 9.0, doubleDirection = 8.0; 
-		Movie.displayMovieRating(script,acting,direction);
-		displayMovieRating(doubleScript, doubleActing, doubleDirection); 
+		Movie m = new Movie(); 
+		int m.script=6, m.acting=9, m.direction=8; 
+		Movie.displayMovieRating(m.script,m.acting,m.direction);
 	}
-	public static void displayMovieRating (double s, double a, double d) { 
-  	System.out.print("The floating-point rating of the movie is ");  
-  	System.out.println(s + a + d);  
-  	return; 
- }  
+	
 }
