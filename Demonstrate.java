89d13260ed7d1eff7e873b4e4d0ed7f7ec8da19f
Adil <adilchhabra7@gmail.com>
2016-03-14 20:51:42 -0400

    Calling Symphony
    Instance method of same name - different class reference - different
    result.

--- Demonstrate.java
+++ Demonstrate.java
@@ -1,8 +1,11 @@
 public class Demonstrate {
 	public static void main(String [] args) {
 		Movie m = new Movie(); 
-		int m.script=6, m.acting=9, m.direction=8; 
+		m.script=6, m.acting=9, m.direction=8; 
+		Symphony s = new Symphony(); 
+  		s.music = 7; s.playing = 8; s.conducting = 5; 
   		System.out.println("The rating of the movie is " + m.rating()); 
+  		System.out.println("The rating of the symphony is " + s.rating()); 
 	}
 	
 }
