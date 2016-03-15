158b699ad7c16197976c283d0353009f1f3c5724
Adil <adilchhabra7@gmail.com>
2016-03-14 20:17:33 -0400

    Declared Instance Variables

--- Movie.java
+++ Movie.java
@@ -1,5 +1,5 @@
 public class Movie {
-	
+	public int script, acting, direction; 
 	public static void displayMovieRating (int s, int a, int d) { 
 	int result = s+a+d;
   	System.out.println("The rating of the movie is: ");
