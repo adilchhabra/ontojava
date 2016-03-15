0071671539989a7c911d09d7d910ed46c1ef5ae0
Adil <adilchhabra7@gmail.com>
2016-03-14 21:01:51 -0400

    Deletion
    Rating of movie is 15.

--- Demonstrate.java
+++ Demonstrate.java
@@ -1,11 +1,7 @@
 public class Demonstrate {
 	public static void main(String [] args) {
 		Movie m = new Movie(); 
-		m.script=6, m.acting=9, m.direction=8; 
-		Symphony s = new Symphony(); 
-  		s.music = 7; s.playing = 8; s.conducting = 5; 
   		System.out.println("The rating of the movie is " + m.rating()); 
-  		System.out.println("The rating of the symphony is " + s.rating()); 
 	}
 	
 }
