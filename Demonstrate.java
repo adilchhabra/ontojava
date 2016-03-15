23ef1bd59f401c1c66eee0203e4cf7ee9195b3c1
Adil <adilchhabra7@gmail.com>
2016-03-14 21:03:50 -0400

    To Call Modifiable Constructor Method

--- Demonstrate.java
+++ Demonstrate.java
@@ -1,6 +1,6 @@
 public class Demonstrate {
 	public static void main(String [] args) {
-		Movie m = new Movie(); 
+		Movie m = new Movie(8,9,6); 
   		System.out.println("The rating of the movie is " + m.rating()); 
 	}
 	
