f72b64250018b020075d0ee7e80be4ba694e51a4
Adil <adilchhabra7@gmail.com>
2016-03-14 21:02:42 -0400

    Assignable Constructor

--- Movie.java
+++ Movie.java
@@ -3,7 +3,10 @@ public class Movie {
  	public Movie(){
  		script = 5; acting = 5; direction = 5;        
  	} 
+ 	 public Movie(int s, int a, int d) {            
+  		script = s; acting = a; direction = d;        
+ 	}      
  	public static int rating () { 
   		return script + acting + direction; 
- } 
+ 	} 
 } 
