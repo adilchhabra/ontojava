a2206a3e343c4e7eb95525e9319396af5b94757a
Adil <adilchhabra7@gmail.com>
2016-03-14 20:46:01 -0400

    Calling Instance Method

--- Demonstrate.java
+++ Demonstrate.java
@@ -2,8 +2,7 @@ public class Demonstrate {
 	public static void main(String [] args) {
 		Movie m = new Movie(); 
 		int m.script=6, m.acting=9, m.direction=8; 
-		System.out.print("The rating of the movie is "); 
-  		System.out.println(Movie.rating(m)); 
+  		System.out.println("The rating of the movie is " + m.rating()); 
 	}
 	
 }
