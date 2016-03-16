3c5b0b11e416da7018cc8824e5048abc5865616f
Adil <adilchhabra7@gmail.com>
2016-03-16 03:24:18 -0400

    Created Class Attraction
    Contains variable to be used in both Movie and Symphony

---
+++ Attraction.java
@@ -0,0 +1,12 @@
+public class Attraction { 
+ // Define instance variable: 
+ public int minutes; 
+ // Define zero-parameter constructor: 
+ public Attraction () { 
+  System.out.println("Calling zero-parameter Attraction constructor"); 
+  minutes = 75; 
+ } 
+ public Attraction (int m) {
+ 	minutes = m;
+ } 
+} 
