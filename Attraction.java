8205915546db31487ffa90f104a2a7e94147d246
Adil <adilchhabra7@gmail.com>
2016-03-16 19:12:01 -0400

    Made Variable Private

--- Attraction.java
+++ Attraction.java
@@ -1,12 +1,18 @@
 public class Attraction { 
  // Define instance variable: 
- public int minutes; 
+ private int minutes; 
  // Define zero-parameter constructor: 
  public Attraction () { 
   System.out.println("Calling zero-parameter Attraction constructor"); 
   minutes = 75; 
  } 
  public Attraction (int m) {
+ 	minutes = m;
+ } 
+ public int getMinutes () {
+ 	return minutes;
+ } 
+ public void setMinutes (int m) {
  	minutes = m;
  } 
 } 
