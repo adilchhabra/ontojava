ec372b70e3239e99041c6e0b74a6af534328ac57
Adil <adilchhabra7@gmail.com>
2016-03-16 03:31:26 -0400

    Defining Super Class
    Make use of extends

--- Movie.java
+++ Movie.java
@@ -1,4 +1,4 @@
-public class Movie {
+public class Movie extends Attraction{
  	public int script, acting, direction;
  	public Movie(){
  		script = 5; acting = 5; direction = 5;        
--- Symphony.java
+++ Symphony.java
@@ -1,4 +1,4 @@
-public class Symphony { 
+public class Symphony extends Attraction { 
  	public int music, playing, conducting; 
  	public int rating () { 
   		return music + playing + conducting; 
